b562a75f5fb57b0d4206dc41371e6c3313ccdd99
sairaj <sairajsrigiri@gmail.com>
2025-07-25 18:12:41 +0530

    DONEgit add .git add .!

--- src/app/api/admin/getGridWithId/route.js
+++ src/app/api/admin/getGridWithId/route.js
@@ -1,47 +1,5 @@
 
 
-// import { connectDB } from "@/dbConnection/ConnectDB";
-// import { NextResponse, NextRequest } from "next/server";
-// import Grid from "@/models/Grid.model";
-// export async function GET(req) {
-//   try {
-//     await connectDB();
-//   } catch (error) {
-//     return NextResponse.json(
-//       { success:false,
-//         message: "Database connection failed",
-//         error: error},
-//       { status: 500 }
-//     );
-//   }
-//   try {
-//     const { searchParams } = new URL(req.url);
-//     const gridId = searchParams.get("gridId");
-
-//     if (!gridId) {
-//       return NextResponse.json(
-//         { success: false, message: "Grid ID is required" },
-//         { status: 400 }
-//       );
-//     }
-
-//     const grid = await Grid.findById(gridId)
-
-//     if (!grid) {
-//       return NextResponse.json(
-//         { success: false, message: "Grid not found" },
-//         { status: 404 }
-//       );
-//     }
-
-//     return NextResponse.json({ success: true, grid }, { status: 200 });
-//   } catch (error) {
-    
-//   }
-// }
-
-
-
 import { connectDB } from "@/dbConnection/ConnectDB";
 import { NextResponse, NextRequest } from "next/server";
 import Grid from "@/models/Grid.model";
